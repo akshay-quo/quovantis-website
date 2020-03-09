62b1153277fbeb3cf548bebffb02a8c81aa087f9
shikha <shikha.baskandi@quovantis.com>
2020-03-09 06:06:41 +0530

    - UI developed for 'what we do' page for desktop & laptop view only (not mobile responsive) - Footer created (not mobile responsive) - some fixes done on 'lucid reality' page

---
+++ pages/what-we-do/what-we-do.js
@@ -0,0 +1,18 @@
+let theButton = document.getElementById('hamburger-menu-btn');
+let bgWhite = document.querySelectorAll('.header-main');
+let expandedMenu = document.querySelectorAll('.expanded-menu');
+let headerListItems = document.getElementsByClassName('header-navigation__wrapper');
+
+theButton.onclick = function () {
+    for(let x of bgWhite) {
+        x.classList.toggle('header-white');
+    }
+    for(let y of expandedMenu) {
+        y.classList.toggle('active');
+    }
+
+    //   if (bgWhite.classList.contains('header-white')) {
+
+    //   }
+    headerListItems[0].classList.toggle('hide-it');
+};
